f7f84c32dbd19a96963c1c85408124e14943226f
VikasShetty <shettyvikas806@gmail.com>
2026-09-22 22:54:09 +0530

    Added Secondary Motion

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -179,12 +179,31 @@ var camera_idle_time := 0.0
 @export_category("Camera Inertia")
 
 @export var look_smoothness := 14.0
-@export var max_camera_yaw_offset := 0.06
-@export var camera_yaw_inertia := 0.003
-@export var yaw_return_speed := 10.0
+@export var max_camera_yaw_offset := 0.085
+@export var camera_yaw_inertia := 0.004
+@export var yaw_return_speed := 7.0
 
 var camera_yaw_offset := 0.0
 
+
+@export_category("Bodycam Camera Inertia")
+
+@export var bodycam_position_lag := 8.0
+@export var bodycam_rotation_lag := 10.0
+
+@export var bodycam_forward_inertia := 0.025
+@export var bodycam_side_inertia := 0.018
+@export var bodycam_vertical_inertia := 0.012
+
+var bodycam_velocity_offset := Vector3.ZERO
+var bodycam_target_offset := Vector3.ZERO
+
+@export_category("Bodycam Secondary Motion")
+
+@export var bodycam_secondary_amount := 0.012
+@export var bodycam_secondary_speed := 2.2
+
+var bodycam_secondary_time := 0.0
 
 # ============================================================
 # CAMERA SHAKE
@@ -565,8 +584,8 @@ func _physics_process(delta: float) -> void:
 	handle_footsteps()
 
 
+	handle_bodycam_inertia(delta)
 	handle_camera_inertia(delta)
-
 	handle_camera_shake(delta)
 
 	handle_weapon_fire(delta)
@@ -694,7 +713,27 @@ func handle_bodycam_motion(delta: float) -> void:
 	)
 
 	var movement_speed: float = horizontal_velocity.length()
+	
+	# ========================================================
+	# BODYCAM SECONDARY MOTION
+	# ========================================================
 
+	bodycam_secondary_time += (
+		delta *
+		bodycam_secondary_speed
+	)
+
+	var secondary_motion := Vector3(
+		sin(bodycam_secondary_time * 0.8)
+		* bodycam_secondary_amount,
+
+		cos(bodycam_secondary_time)
+		* bodycam_secondary_amount,
+
+		sin(bodycam_secondary_time * 0.5)
+		* bodycam_secondary_amount
+	)
+	
 	var is_moving: bool = (
 		movement_speed > 0.15
 	)
@@ -911,6 +950,8 @@ func handle_bodycam_motion(delta: float) -> void:
 			bob_z
 		)
 		+ idle_bob
+		+ bodycam_velocity_offset
+		+secondary_motion
 	)
 
 
@@ -2477,3 +2518,55 @@ func get_bone_damage(bone_name: String) -> float:
 		return 10.0
 
 	return 25.0
+
+func handle_bodycam_inertia(delta: float) -> void:
+
+	var horizontal_velocity := Vector3(
+		velocity.x,
+		0.0,
+		velocity.z
+	)
+
+	var local_velocity := (
+		global_transform.basis.inverse() *
+		horizontal_velocity
+	)
+
+	# ========================================================
+	# BODY MOVEMENT CAUSED BY ACCELERATION
+	# ========================================================
+
+	var target_offset := Vector3(
+		-local_velocity.x * bodycam_side_inertia,
+		0.0,
+		-local_velocity.z * bodycam_forward_inertia
+	)
+
+	# Small vertical movement from speed
+	target_offset.y = (
+		-local_velocity.length() *
+		bodycam_vertical_inertia
+	)
+
+
+	# ========================================================
+	# SMOOTH BODY RESPONSE
+	# ========================================================
+
+	var smoothing := (
+		1.0 -
+		exp(
+			-bodycam_position_lag * delta
+		)
+	)
+
+	bodycam_target_offset = bodycam_target_offset.lerp(
+		target_offset,
+		smoothing
+	)
+
+
+	bodycam_velocity_offset = bodycam_velocity_offset.lerp(
+		bodycam_target_offset,
+		smoothing
+	)
